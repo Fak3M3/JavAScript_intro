0113069a09bc90c1f68c79689334f0d05c615356
FUKINGKING <sanchezzamorareynathan@gmail.com>
2025-03-20 20:09:23 -0600

    :heavy_check_mark: mi primer codigo

---
+++ 01.Estructuras_de_Control/main.js
@@ -0,0 +1,11 @@
+// trabajaremos nuestra logica
+
+// escribiremos una calculadora de edad
+
+let age = 10;
+
+if(age >= 18){
+    console.log("Estas grandote");
+}else{
+    console.log("Estas chirris");
+}

3fe858271cdd9899a278729fec9832d332db2692
FUKINGKING <sanchezzamorareynathan@gmail.com>
2025-03-23 22:08:04 -0600

    :tada: ejemplos de js

---
+++ Tareas/Tareas-1/tipo-de-dato.js
@@ -0,0 +1,15 @@
+
+
+console.log(typeof 42);
+
+console.log(typeof -666);
+
+console.log(typeof false);
+
+console.log(typeof Reynathan);
+
+console.log(typeof undefined);
+
+console.log(typeof null);
+
+console.log(typeof "");

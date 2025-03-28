66349487e7dc095e948816838b3f8291e1586185
FUKINGKING <sanchezzamorareynathan@gmail.com>
2025-03-27 18:23:18 -0600

    :finnadie: tarea hecha

---
+++ Tareas/Tareas-2/cajero.js
@@ -0,0 +1,25 @@
+const userName = window.prompt("BIENVENIDO INGRESA TU NOMBRE");
+let saldo = 1000;
+let retiro;
+let seguir = true;
+
+do {
+  let entrada = prompt(`Hola ${userName}, tu saldo es: $${saldo}. Ingresa la cantidad a retirar:`);
+
+  if (entrada !== null && entrada !== "") {
+    retiro = parseInt(entrada);
+
+    if (retiro <= saldo && retiro > 0) {
+      saldo = saldo - retiro;
+      console.log(`Retiro exitoso, ${userName}. Saliendo de la app.`);
+    } else {
+      console.log(`Fondos insuficientes, ${userName}. Saliendo de la app.`);
+    }
+
+    seguir = false; // ya sea válido o no, termina
+  } else {
+    console.log(`No ingresaste una cantidad, ${userName}. Saliendo de la app.`);
+    seguir = false;
+  }
+
+} while (seguir);

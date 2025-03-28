40df58f83c305a3a44e4f1b0cf37ebecbb09dde7
FUKINGKING <sanchezzamorareynathan@gmail.com>
2025-03-27 21:30:30 -0600

    :dragon_face: nuevo tema broda

---
+++ 03.Funciones/funciones.js
@@ -0,0 +1,18 @@
+// funcion sin parametros
+
+function getGreeting(){
+    console.log("hola bobos");
+};
+getGreeting();
+
+
+// funcion con parametros
+
+function getSUM(num1,num2){
+    return num1+num2; //devuelve un valor de salida, ademas , lo guarda
+};
+
+console.log(`La suma es: ${getSUM(2,2)} 🌞`);
+
+
+
